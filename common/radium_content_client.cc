3b15190c1b1d86a832826ad0259bde63aa93e12f
weidongliu <weidongliu@tencent.com>
2024-10-27 16:04:54 +0800

    支持 render 启动

--- common/radium_content_client.cc
+++ common/radium_content_client.cc
@@ -4,7 +4,67 @@
 
 #include "radium/common/radium_content_client.h"
 
+#include "components/crash/core/common/crash_key.h"
+#include "config/gpu_util.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
+
 RadiumContentClient::RadiumContentClient() = default;
 RadiumContentClient::~RadiumContentClient() = default;
 
+void RadiumContentClient::SetActiveURL(const GURL& url,
+                                       std::string top_origin) {
+  static crash_reporter::CrashKeyString<1024> active_url("url-chunk");
+  active_url.Set(url.possibly_invalid_spec());
+
+  // Use a large enough size for Origin::GetDebugString.
+  static crash_reporter::CrashKeyString<128> top_origin_key("top-origin");
+  top_origin_key.Set(top_origin);
+}
+
+void RadiumContentClient::SetGpuInfo(const gpu::GPUInfo& gpu_info) {
+  gpu::SetKeysForCrashLogging(gpu_info);
+}
+
 void RadiumContentClient::AddAdditionalSchemes(Schemes* schemes) {}
+
+std::u16string RadiumContentClient::GetLocalizedString(int message_id) {
+  return l10n_util::GetStringUTF16(message_id);
+}
+
+std::u16string RadiumContentClient::GetLocalizedString(
+    int message_id,
+    const std::u16string& replacement) {
+  return l10n_util::GetStringFUTF16(message_id, replacement);
+}
+
+bool RadiumContentClient::HasDataResource(int resource_id) const {
+  return ui::ResourceBundle::GetSharedInstance().HasDataResource(resource_id);
+}
+
+std::string_view RadiumContentClient::GetDataResource(
+    int resource_id,
+    ui::ResourceScaleFactor scale_factor) {
+  return ui::ResourceBundle::GetSharedInstance().GetRawDataResourceForScale(
+      resource_id, scale_factor);
+}
+
+base::RefCountedMemory* RadiumContentClient::GetDataResourceBytes(
+    int resource_id) {
+  return ui::ResourceBundle::GetSharedInstance().LoadDataResourceBytes(
+      resource_id);
+}
+
+std::string RadiumContentClient::GetDataResourceString(int resource_id) {
+  return ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
+      resource_id);
+}
+
+gfx::Image& RadiumContentClient::GetNativeImageNamed(int resource_id) {
+  return ui::ResourceBundle::GetSharedInstance().GetNativeImageNamed(
+      resource_id);
+}
+
+std::string RadiumContentClient::GetProcessTypeNameInEnglish(int type) {
+  return "Unknown";
+}
--- common/radium_content_client.h
+++ common/radium_content_client.h
@@ -16,7 +16,20 @@ class RadiumContentClient : public content::ContentClient {
   ~RadiumContentClient() override;
 
  private:
+  void SetActiveURL(const GURL& url, std::string top_origin) override;
+  void SetGpuInfo(const gpu::GPUInfo& gpu_info) override;
   void AddAdditionalSchemes(Schemes* schemes) override;
+  std::u16string GetLocalizedString(int message_id) override;
+  std::u16string GetLocalizedString(int message_id,
+                                    const std::u16string& replacement) override;
+  bool HasDataResource(int resource_id) const override;
+  std::string_view GetDataResource(
+      int resource_id,
+      ui::ResourceScaleFactor scale_factor) override;
+  base::RefCountedMemory* GetDataResourceBytes(int resource_id) override;
+  std::string GetDataResourceString(int resource_id) override;
+  gfx::Image& GetNativeImageNamed(int resource_id) override;
+  std::string GetProcessTypeNameInEnglish(int type) override;
 };
 
 #endif  // RADIUM_COMMON_RADIUM_CONTENT_CLIENT_H_
