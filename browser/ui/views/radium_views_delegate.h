290e0379030804dc20bf2e3b0be8badf3a2bfe6b
weidongliu <weidongliu@tencent.com>
2025-01-16 00:04:22 +0800

    [Windows] Fixed the black border at the bottom of the maximized window.

--- browser/ui/views/radium_views_delegate.h
+++ browser/ui/views/radium_views_delegate.h
@@ -5,6 +5,8 @@
 #ifndef RADIUM_BROWSER_UI_VIEWS_RADIUM_VIEWS_DELEGATE_H_
 #define RADIUM_BROWSER_UI_VIEWS_RADIUM_VIEWS_DELEGATE_H_
 
+#include <map>
+
 #include "ui/views/views_delegate.h"
 
 class ScopedKeepAlive;
@@ -27,6 +29,12 @@ class RadiumViewsDelegate : public views::ViewsDelegate {
       const std::string& window_name,
       gfx::Rect* bounds,
       ui::mojom::WindowShowState* show_state) const override;
+
+#if BUILDFLAG(IS_WIN)
+  int GetAppbarAutohideEdges(HMONITOR monitor,
+                             base::OnceClosure callback) override;
+#endif
+
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
   bool WindowManagerProvidesTitleBar(bool maximized) override;
 #endif
@@ -42,6 +50,17 @@ class RadiumViewsDelegate : public views::ViewsDelegate {
 #endif
 
  private:
+#if BUILDFLAG(IS_WIN)
+  typedef std::map<HMONITOR, int> AppbarAutohideEdgeMap;
+
+  // Callback on main thread with the edges. |returned_edges| is the value that
+  // was returned from the call to GetAutohideEdges() that initiated the lookup.
+  void OnGotAppbarAutohideEdges(base::OnceClosure callback,
+                                HMONITOR monitor,
+                                int returned_edges,
+                                int edges);
+#endif
+
   views::NativeWidget* CreateNativeWidget(
       views::Widget::InitParams* params,
       views::internal::NativeWidgetDelegate* delegate);
@@ -53,6 +72,15 @@ class RadiumViewsDelegate : public views::ViewsDelegate {
 
   // Prevents BrowserProcess teardown while |ref_count_| is non-zero.
   std::unique_ptr<ScopedKeepAlive> keep_alive_;
+
+#if BUILDFLAG(IS_WIN)
+  AppbarAutohideEdgeMap appbar_autohide_edge_map_;
+  // If true we're in the process of notifying a callback from
+  // GetAutohideEdges().start a new query.
+  bool in_autohide_edges_callback_ = false;
+
+  base::WeakPtrFactory<RadiumViewsDelegate> weak_factory_{this};
+#endif
 };
 
 #endif  // RADIUM_BROWSER_UI_VIEWS_RADIUM_VIEWS_DELEGATE_H_
