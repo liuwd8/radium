2df96c5f974ccdf2ce9c26a82e6c9c4505392c99
weidongliu <weidongliu@tencent.com>
2025-05-13 14:11:52 +0800

    Fixed the issue of fast_build process remaining and freezing.

--- build/android/fast_local_dev_server.py
+++ build/android/fast_local_dev_server.py
@@ -9,5 +9,10 @@ import sys
 
 SRC_DIR = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
 
-args = [sys.executable, f"{SRC_DIR}/build/android/fast_local_dev_server.py"]
-sys.exit(subprocess.call(args))
+sys.path.insert(0, SRC_DIR)
+
+from build.android.fast_local_dev_server import main, _exception_hook
+
+if __name__ == '__main__':
+  sys.excepthook = _exception_hook
+  sys.exit(main())
